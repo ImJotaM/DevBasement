d44ffe950e1841f711f9e5db2a2c950b81ae0a29
ImJotaM <jo0209.letras@gmail.com>
2026-05-13 21:16:26 -0300

    update: test

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('delete/', views.login_view, name='signup'),
+    path('update/', views.login_view, name='signup'),
 ]
